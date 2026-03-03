52ace78d66663473320ca55a4d59d9ee81947261
Renke Brixel <renke.brixel@gmail.com>
2026-03-03 16:41:24 +0100

    add react-router starter-code

---
+++ module05-building-apps-with-react/01-0-react-router-starter-code/src/App.jsx
@@ -0,0 +1,16 @@
+import AlphaCentauri from './components/AlphaCentauri';
+import Header from './components/Header';
+import Stars from './components/Stars';
+
+function App() {
+  return (
+    <div className='body'>
+      <Header />
+      <Stars />
+      {/* <AlphaCentauri /> */}
+      <footer>© footerbla</footer>
+    </div>
+  );
+}
+
+export default App;
