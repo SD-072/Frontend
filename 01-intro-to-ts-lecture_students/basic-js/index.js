b734c73caf00e44299122072c07de1adce4d61b2
Renke Brixel <renke.brixel@gmail.com>
2026-03-09 16:48:31 +0100

    remove ts intro

--- 01-intro-to-ts-lecture_students/basic-js/index.js
+++
@@ -1,6 +0,0 @@
-let num = 6;
-num = 'Not anymore!';
-
-num.forEach((element) => {
-  console.log(element);
-});
--- 01-intro-to-ts-lecture_students/basic-js/ts-setup/src/main.js
+++
@@ -1,6 +0,0 @@
-console.log('Sanity check!');
-var num = 6;
-num = 'Not anymore!';
-num.forEach(function (element) {
-    console.log(element);
-});
