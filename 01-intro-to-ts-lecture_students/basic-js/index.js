050f621b3085eb06252bde85c9d87c4ad53f7647
Renke Brixel <renke.brixel@gmail.com>
2026-03-09 16:44:08 +0100

    add personal diary

---
+++ 01-intro-to-ts-lecture_students/basic-js/index.js
@@ -0,0 +1,6 @@
+let num = 6;
+num = 'Not anymore!';
+
+num.forEach((element) => {
+  console.log(element);
+});
---
+++ 01-intro-to-ts-lecture_students/basic-js/ts-setup/src/main.js
@@ -0,0 +1,6 @@
+console.log('Sanity check!');
+var num = 6;
+num = 'Not anymore!';
+num.forEach(function (element) {
+    console.log(element);
+});
